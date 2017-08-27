13cb364b3a560917eb5d19863c5f74490bcc578d
Ray Donnelly <mingw.android@gmail.com>
2017-08-27 17:30:22 +0100

    Generalize logging

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-./configure --prefix=$PREFIX --build=$BUILD
-make
+./configure --prefix=${PREFIX}  \
+            --host=${HOST}      \
+            --build=${BUILD}
+make -j${CPU_COUNT} ${VERBOSE_AT}
 make tests
 make install
